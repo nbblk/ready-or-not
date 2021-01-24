3c46db02c8eaf401af50f4a3bedf16f38ece7868
blks <nbblks@gmail.com>
2021-01-24 18:13:44 +0900

    Add searchbar to main

---
+++ frontend/src/components/Searchbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const searchbar = (props) => {
+  return (
+    <div className="w-full flex flex-col md:flex-row justify-center items-center">
+      <label className="block md:inline mx-6 my-6 md:my-0 font-archivo text-center md:text-auto text-xl">Search</label>
+      <input
+        className="block md:inline py-3 px-6 w-3/4 md:w-1/4 h-10 border border-black rounded-full shadow-bold"
+        type="text"
+        placeholder="Enter something..."
+      />
+    </div>
+  );
+};
+
+export default searchbar;
