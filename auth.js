5d758859f3f63f8357a5c1ae020092b445bb3441
blks <nbblks@gmail.com>
2021-01-27 22:18:05 +0900

    Refactor redundant auth code

--- auth.js
+++ auth.js
@@ -6,46 +6,74 @@ const GITHUB_OAUTH_CLIENT_ID = process.env.GITHUB_OAUTH_CLIENT_ID;
 const GITHUB_OAUTH_CLIENT_SECRET = process.env.GITHUB_OAUTH_CLIENT_SECRET;
 const authClient = new OAuth2Client(process.env.GOOGLE_OAUTH_CLIENT_ID);
 
-const authenticateGithub = (code) => {
-  axios
-    .post("https://github.com/login/oauth/access_token", {
-      headers: { Accept: "application/json" },
-      client_id: GITHUB_OAUTH_CLIENT_ID,
-      client_secret: GITHUB_OAUTH_CLIENT_SECRET,
-      code: code,
-    })
-    .then(async (response) => {
-      const queryStr = response.data;
-      const accessToken = queryStr.split("=")[1].split("&")[0];
-      const user = await fetchUser(accessToken);
-      return user;
-    });
+const login = async (oauthType, token) => {
+  let user;
+  if (oauthType === "google") {
+    user = await loginWithGoogle(oauthType, token);
+  }
+  if (oauthType === "github") {
+    user = await loginWithGithub({ oauthType, token });
+  }
+  return user, token;
 };
 
-const authenticateGoogle = async (token) => {
+const authorize = async (oauthType, token) => {
+  if (oauthType === "google") {
+    await verifyGoogleToken(token);
+  }
+  if (oauthType === "github") {
+    await verifyGithubToken(token);
+  }
+};
+
+const verifyGoogleToken = async (token) => {
   const ticket = await authClient.verifyIdToken({
     idToken: token,
     audience: process.env.GOOGLE_OAUTH_CLIENT_ID,
   });
+  return { ticket, token };
+};
 
-  const { name, email, picture } = ticket.getPayload();
-  const user = await db.upsertUser({ name, email, picture });
+const loginWithGoogle = async (oauthType, token) => {
+  const verified = await verifyGoogle(token);
+  const { name, email, picture } = verified.ticket.getPayload();
+  const accessToken = verified.token;
+  const user = await db.upsertUser({
+    oauthType,
+    accessToken,
+    name,
+    email,
+    picture,
+  });
 
   return user;
 };
 
-const fetchUser = (accessToken) => {
-  axios
-    .get("https://api.github.com/user", {
-      headers: {
-        Authorization: `token ${accessToken}`,
-        Accept: "application/vnd.github.v3+json",
-      },
-    })
-    .then(async (response) => {
-      const user = await db.upsertUser(response);
-      return user;
-    });
+const verifyGithubToken = async (tempCode) => {
+  const response = await axios.post(
+    "https://github.com/login/oauth/access_token",
+    {
+      headers: { Accept: "application/json" },
+      client_id: GITHUB_OAUTH_CLIENT_ID,
+      client_secret: GITHUB_OAUTH_CLIENT_SECRET,
+      code: tempCode,
+    }
+  );
+  const queryStr = response.data;
+  const accessToken = queryStr.split("=")[1].split("&")[0];
+  return accessToken;
 };
 
-module.exports = { authenticateGithub, authenticateGoogle };
+const loginWithGithub = async (oauthType, code) => {
+  const accessToken = await verifyGithub({ oauthType, code });
+  const response = await axios.get("https://api.github.com/user", {
+    headers: {
+      Authorization: `token ${accessToken}`,
+      Accept: "application/vnd.github.v3+json",
+    },
+  });
+  const user = await db.upsertUser(response);
+  return user;
+};
+
+module.exports = { login, authorize };
--- index.js
+++ index.js
@@ -19,16 +19,52 @@ app.listen(PORT, () =>
   console.log(`ready-or-not server listening at http://localhost:${PORT}`)
 );
 
-app.post("/api/v1/auth/github", jsonParser, async (req, res) => {
-  const { code } = req.body;
-  const user = await auth.authenticateGithub(code);
-  res.status(201);
+const authorize = async (req, res, next) => {
+  const oauthType = req.headers.cookie.split('; ')[0].split('=')[1];
+  const token = req.headers["x-access-token"];
+  try {
+    await auth.authorize(oauthType, token);    
+  } catch (error) {
+    res.status(401);
+  }
+  next();
+};
+
+app.post("/api/v1/auth/:oauthType", jsonParser, async (req, res) => {
+  const oauthType = req.params.oauthType;
+  const token = req.body.token;
+  try {
+    await auth.login(oauthType, token);
+  } catch (error) {
+    res.status(401);
+  }  
+  res.status(308);
+  res.cookie('oauth', oauthType, {maxAge: 10800});
   res.json(user);
 });
 
-app.post("/api/v1/auth/google", jsonParser, async (req, res) => {
-  const { token } = req.body;
-  const user = await auth.authenticateGoogle(token);
+app.post("/api/v1/article/new", jsonParser, authorize, async (req, res) => {
+  const token = req.headers["x-access-token"];
+  const _id = req.body._id;
+  const { url, tags, due } = req.body.resource;
+  const articles = await db.upsertArticle({ token, _id, url, tags, due });
+  res.status(200);
+  res.json(articles);
+});
+
+app.get("/api/v1/articles/:_id", jsonParser, authorize, async (req, res) => {
+  const _id = req.params._id;
+  const articles = await db.fetchArticles(_id);
+  res.status(200);
+  res.json(articles);
+});
+
+app.delete("/api/v1/articles/:_id", jsonParser, authorize, async (req, res) => {
+  await db.deleteArticle({ _id: req.params._id, articleId: req.body._id });
+  res.status(204);
+});
+
+app.put("/api/v1/archive/:_id", jsonParser, authorize, async (req, res) => {
+  await db.upsertArchive({ _id: req.params._id, article: req.body });
   res.status(201);
-  res.json(user);
 });
