5d0fc1c4e961bad98e28d97ddaab9a5efc241fa6
blks <nbblks@gmail.com>
2021-02-04 21:05:32 +0900

    Add Archive container

--- frontend/src/components/Archive.js
+++ frontend/src/components/Archive.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import NewArticleIcon from "./NewArticle";
 import Article from "./Article";
 
 const user = JSON.parse(sessionStorage.getItem("user"));
 
-class Articles extends Component {
+class Archive extends Component {
   state = {
     articles: [],
   };
 
   async fetchData() {
     const response = await fetch(
-      `http://localhost:8080/api/v1/articles?uid=${user._id}&oauth=${user.oauth}`,
+      `http://localhost:8080/api/v1/archive?uid=${user._id}&oauth=${user.oauth}`,
       {
         method: "GET",
         credentials: "include",
@@ -21,7 +20,7 @@ class Articles extends Component {
       }
     );
     const list = await response.json();
-    if (list) this.setState({ articles: list[0].articles });
+    if (list) this.setState({ articles: list[0].archived });
   }
 
   findArticle(_id) {
@@ -33,6 +32,7 @@ class Articles extends Component {
       }
       return idx;
     });
+
     return idx;
   }
 
@@ -47,33 +47,13 @@ class Articles extends Component {
     }
   }
 
-  async handleArchive(article) {
-    const repsonse = await fetch(
-      `http://localhost:8080/api/v1/archive?uid=${user._id}&oauth=${user.oauth}`,
-      {
-        method: "PUT",
-        body: JSON.stringify(article),
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Access-Token": `${user.token}`,
-        },
-      }
-    );
-    if (repsonse.status === 201) {
-      await this.updateArticle(article._id);
-    } 
-  };
-
   handleAddNote = () => {
     this.props.history.push("/notes");
   };
 
   async handleDelete(_id) {
-    const user = JSON.parse(sessionStorage.getItem("user"));
-
     fetch(
-      `http://localhost:8080/api/v1/articles?uid=${user._id}&oauth=${user.oauth}`,
+      `http://localhost:8080/api/v1/archive?uid=${user._id}&oauth=${user.oauth}`,
       {
         method: "DELETE",
         body: JSON.stringify({ _id: _id }),
@@ -86,13 +66,13 @@ class Articles extends Component {
     )
       .then(async (response) => {
         if (response.status === 200) {
-         await this.updateArticle(_id);
+          await this.updateArticle(_id);
         }
       })
       .catch((error) => {
         console.error(error);
       });
-  };
+  }
 
   componentDidMount() {
     this.fetchData();
@@ -100,8 +80,7 @@ class Articles extends Component {
 
   render() {
     return (
-      <section className="w-full h-full p-10 flex flex-col flex-wrap md:flex-row justify-center content-center">
-        <NewArticleIcon />
+      <section>
         {this.state.articles
           ? this.state.articles.map((article, index) => {
               return (
@@ -113,7 +92,6 @@ class Articles extends Component {
                   tags={article.tags}
                   title={article.title}
                   image={article.image}
-                  archive={() => this.handleArchive(article)}
                   addNote={() => this.handleAddNote(article._id)}
                   delete={() => this.handleDelete(article._id)}
                 />
@@ -125,4 +103,4 @@ class Articles extends Component {
   }
 }
 
-export default Articles;
+export default Archive;
--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,66 +1,21 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import ArticleIcons from "./ArticleIcons";
+import TagItem from "./TagItem";
 
 const Article = (props) => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  const accessToken = user.auth.token;
-
-  const history = useHistory();
-
-  const handleArchive = () => {
-    fetch(`http://localhost:8080/api/v1/archive/${user._id}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        _id: props._id,
-        url: props.url,
-        tags: props.tags,
-        due: props.due,
-      }),
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Access-Token": `${accessToken}`,
-      },
-    })
-      .then((response) => {
-        alert("Saved!");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const handleAdd = () => {
-    history.push("/notes");
-  };
-
-  const handleDelete = () => {
-    fetch(`http://localhost:8080/api/v1/articles/${user._id}`, {
-      method: "DELETE",
-      body: JSON.stringify({ _id: props._id }),
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Access-Token": `${accessToken}`,
-      },
-    }).catch((error) => {
-      console.error(error);
-    });
-  };
-
   return (
-    <article className="w-full md:w-1/4 h-56 md:h-48 m-5 border border-solid border-black bg-transparent rounded">
+    <article className="w-full md:w-1/3 m-5 p-3 border border-solid border-black bg-white rounded">
       <div className="w-auto h-2/3 bg-transparent overflow-hidden">
         <ArticleIcons
-          archive={() => handleArchive()}
-          add={() => handleAdd()}
-          delete={() => handleDelete()}
+          archive={props.archive}
+          add={props.addNote}
+          delete={props.delete}
         />
-        <img className="w-full h-full object-scale-down" src="" alt="img" />
+        <img className="w-full h-full object-cover" src={props.image} alt="img" />
       </div>
-      <h1 className="h-1/3 p-3 text-2xl md:text-lg text-left font-archivo text-black">
-        {props.url}
+      {/* <TagItem tags={props.tags}/> */}
+      <h1 className="h-1/3 m-5 text-2xl md:text-lg text-left font-archivo text-black text-left">
+        {props.title}
       </h1>
     </article>
   );
