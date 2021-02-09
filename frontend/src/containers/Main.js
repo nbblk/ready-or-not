b17e9574360f0cc6eaa70db19761293b508e79dc
blks <nbblks@gmail.com>
2021-02-09 21:57:06 +0900

    Add useEffect hook to Main component for fetching

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -7,7 +7,37 @@ function Main(props) {
   const [search, setSearch] = useState({
     keyword: null,
     result: [],
+    submit: false
   });
+
+  React.useEffect(() => {
+    if (search.submit) { 
+      const fetchResult = async () => {
+        await fetch(
+          `http://localhost:8080/api/v1/search?uid=${user._id}&oauth=${user.oauth}&keyword=${search.keyword}`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "X-Access-Token": `${user.token}`,
+            },
+          }
+        )
+        .then(async (response) => {
+          const result = await response.json();
+          await setSearch({
+            ...search,
+            result: result[0].articles,
+            submit: false,
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      };
+      fetchResult();
+    } 
+  }, [search]);
 
   const handleInputChange = async (event) => {
     await setSearch({ ...search, keyword: event.target.value.trim() });
@@ -15,22 +45,8 @@ function Main(props) {
 
   const handleEnter = async (event) => {
     if (event.keycode === 13 || event.key === "Enter") {
-      await fetchResult(search.keyword);
+      await setSearch({ ...search, submit: true });
     }
-  };
-
-  const fetchResult = async (keyword) => {
-    const result = await fetch(
-      `http://localhost:8080/api/v1/search?uid=${user._id}&oauth=${user.oauth}&keyword=${keyword}`,
-      {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          "X-Access-Token": `${user.token}`,
-        },
-      }
-    );
-    await setSearch({ ...search, result: result });
   };
 
   return (
