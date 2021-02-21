d66f5adfd9047fb27fd543641d09059019ec1a72
blks <nbblks@gmail.com>
2021-02-21 21:12:29 +0900

    Change Main to class component

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,62 +1,65 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Searchbar from "../components/Searchbar";
+import ArticleContainer from "./ArticleContainer";
 
-const user = JSON.parse(sessionStorage.getItem("user"));
-
-function Main(props) {
-  const [search, setSearch] = useState({
+class Main extends Component {
+  state = {
     keyword: null,
     result: [],
-    submit: false
-  });
-
-  React.useEffect(() => {
-    if (search.submit) { 
-      const fetchResult = async () => {
-        await fetch(
-          `http://localhost:8080/api/v1/search?uid=${user._id}&oauth=${user.oauth}&keyword=${search.keyword}`,
-          {
-            method: "GET",
-            credentials: "include",
-            headers: {
-              "X-Access-Token": `${user.token}`,
-            },
-          }
-        )
-        .then(async (response) => {
-          const result = await response.json();
-          await setSearch({
-            ...search,
-            result: result[0].articles,
-            submit: false,
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      };
-      fetchResult();
-    } 
-  }, [search]);
-
-  const handleInputChange = async (event) => {
-    await setSearch({ ...search, keyword: event.target.value.trim() });
+    submit: false,
   };
 
-  const handleEnter = async (event) => {
+  async fetchResult() {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    await fetch(
+      `http://localhost:8080/api/v1/search?uid=${user._id}&oauth=${user.oauth}&keyword=${this.state.keyword}`,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "X-Access-Token": `${user.token}`,
+        },
+      }
+    )
+      .then(async (response) => {
+        const result = await response.json();
+        await this.setState({
+          ...this.state,
+          result: result,
+          submit: false,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  componentDidUpdate() {
+    if (this.state.submit) {
+      this.fetchResult();
+    }
+  }
+
+  handleInputChange = event => {
+    this.setState({ ...this.state, keyword: event.target.value.trim() });
+  };
+
+  handleEnter = event => {
     if (event.keycode === 13 || event.key === "Enter") {
-      await setSearch({ ...search, submit: true });
+      this.setState({ ...this.state, submit: true });
     }
   };
 
-  return (
-    <main className="w-full h-full py-20 md:py-40 bg-beige-yellowish flex flex-col justify-center content-center">
-      <Searchbar
-        change={(event) => handleInputChange(event)}
-        keydown={(event) => handleEnter(event)}
-      />
-      {props.children}
-    </main>
-  );
+  render(props) {
+    return (
+      <main className="w-full h-full py-20 md:py-40 bg-beige-yellowish flex flex-col justify-center items-center">
+        <Searchbar
+          change={(event) => this.handleInputChange(event)}
+          keydown={(event) => this.handleEnter(event)}
+        />
+        <ArticleContainer articles={this.state.result}/>
+      </main>
+    );
+  }
 }
 export default Main;
