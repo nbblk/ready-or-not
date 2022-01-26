2f8ac89043c7dfd3290d2f09ef396cdaaabfd9e9
nbblks <nbblks@gmail.com>
2022-01-26 10:58:27 +0900

    fixup! fixup! add cors whitelist

--- backend/index.js
+++ backend/index.js
@@ -72,7 +72,7 @@ app.use(bodyParser.json());
 app.use(morgan("dev")); // logging
 app.use(
   cors({
-    origin: "https://ready-or-not.xyz", // ["https://testron.com:3000", "https://localhost:3000", "https://ready-or-not.xyz"],
+    origin: true,
     credentials: true,
   })
 );
